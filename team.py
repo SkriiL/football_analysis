14bbb2627b0ba0b70d094559b6a1a6d19d2dcde7
SkriiL <flx.grimm@gmail.com>
2018-06-26 14:21:47 +0200

    #v0.2 fixed some errors

--- team.py
+++ team.py
@@ -49,6 +49,9 @@ class Team:
         c = Quotes(self, team2)
         c_dict = c.fetch()
 
+        if c_dict.get(self.name) is None:
+            return
+
         self.calculate_predicted()
         team2.calculate_predicted()
         sum = 0
